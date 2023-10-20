78a6983a0e7e1dc73851a0df0abc21a551fb50f6
Reyna May <roma.reynamayA@gmail.com>
2023-10-20 17:22:24 -0600

    Add add-product form

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,7 +1,40 @@
 export default function AddProductPage() {
-  return(
+  return (
     <div>
-      <h1 className="text-lg">Add Product</h1>
+      <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+      <form>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input input-bordered w-full mb-3"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered w-full mb-3"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="input input-bordered w-full mb-3"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input input-bordered w-full mb-3"
+        />
+        <button
+          className="btn btn-primary btn-block"
+          type="submit">
+          Add Product
+        </button>
+      </form>
     </div>
-  )
+  );
 }
