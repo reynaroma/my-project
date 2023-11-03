f211f7b1a8ed3d16c50a8e20aa4de3d07c03da63
Reyna May <roma.reynamayA@gmail.com>
2023-11-02 22:36:37 -0600

    Add main product on the first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,21 @@ export default async function Home() {
 
   return (
     <div>
-      <ProductCard product={products[2]} />
+      <div className='hero rounded-xl bg-base-200'>
+        <div className='hero-content flex-col lg:flex-row'>
+          <Image
+            src={products[0].imageUrl}
+            alt={products[0].name}
+            width={400}
+            height={800}
+            className='w-full max-w-sm rounded-lg shadow-2xl'
+            priority
+          />
+          <div>
+            <h1 className='text-5xl font-bold'>{products[0].name}</h1>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
