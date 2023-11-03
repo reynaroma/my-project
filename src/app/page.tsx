765aa80cf0a01797c14bcf1eddc636e24c1e02d7
Reyna May <roma.reynamayA@gmail.com>
2023-11-02 21:28:30 -0600

    Add product card for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import ProductCard from '@/components/ProductCard'
+import prisma from '@/lib/db/prisma'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  // get products from database
+  const products = await prisma.product.findMany({
+    orderBy: { id: 'desc' }
+  })
+
   return (
-    
+    <div>
+      <ProductCard product={products[0]} />
+    </div>
   )
 }
