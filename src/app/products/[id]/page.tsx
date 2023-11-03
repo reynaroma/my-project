9cb8659aa8d649d180021cb68263f708cbbcfae6
Reyna May <roma.reynamayA@gmail.com>
2023-11-03 17:08:11 -0600

    Add details for single product

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,7 @@
+import PriceTag from "@/components/PriceTag";
 import prisma from "@/lib/db/prisma";
-import { Product } from "@prisma/client";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 // setup the props for the page
 interface ProductPageProps {
@@ -17,4 +19,24 @@ export default async function ProductPage(
   const product = await prisma.product.findUnique({
     where: { id }
   })
+
+  if (!product) notFound();
+  // render product page
+  return (
+    <div className="flex flex-col lg:flex-row">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price}  className="mt-4"/>
+        <p className="py-6">{product.description}</p>
+      </div>
+    </div>
+  )
 }
