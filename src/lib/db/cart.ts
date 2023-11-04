1ea3a58579b7bb8572e3d3edf3792d8a9934e1b0
Reyna May <roma.reynamayA@gmail.com>
2023-11-03 19:03:56 -0600

    Add type in the cart

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,6 +1,18 @@
 //  functions we use in server actions
+import { Cart, Prisma } from "@prisma/client";
 import prisma from "./prisma";
 import {cookies} from "next/headers"
+
+// create the type for the cart with products
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: { items: { include: { product: true } } };
+}>
+
+// the cart type; add what type
+export type ShoppingCart = CartWithProducts & {
+  size: number,
+  subtotal: number,
+};
 
 // gets the cart from database
 export async function getCart() {
@@ -8,8 +20,22 @@ export async function getCart() {
   const localCartId = cookies().get("localCartId")?.value;
   const cart = localCartId? await prisma.cart.findUnique({
     where: {id: localCartId},
-    include: { items}
+    // include the items in the cart
+    include: { items: { include: {product: true}  } }
   })
+  : null;
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    // 
+    ...cart,
+    // calculate the size of the cart
+    size: cart.items.reduce((acc, items) => acc + items.quantity, 0),
+    subtotal: cart.items.reduce((acc, item) => acc + item.quantity * item.product.price, 0),
+  }
 }
 
 // create an empty cart
