a44fe7ed43e38c6724eaf3cf0965997e54c8697e
Reyna May <roma.reynamayA@gmail.com>
2023-11-03 18:28:12 -0600

    Add the AddToCart component to the product[id] page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { cache } from "react";
+import AddToCartButton from "../AddToCart";
 
 // setup the props for the page
 interface ProductPageProps {
@@ -65,6 +66,7 @@ export default async function ProductPage(
         <h1 className="text-5xl font-bold">{product.name}</h1>
         <PriceTag price={product.price} className="mt-4" />
         <p className="py-6">{product.description}</p>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
