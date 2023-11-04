28fc89571df1a53ba063ab346a1a0cdc7f0cedf0
Reyna May <roma.reynamayA@gmail.com>
2023-11-03 20:25:00 -0600

    Add incrementProductQuantity to the AddToCartButton component

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import { cache } from "react";
 import AddToCartButton from "../AddToCart";
+import { incrementProductQuantity } from "../actions";
 
 // setup the props for the page
 interface ProductPageProps {
@@ -66,7 +67,9 @@ export default async function ProductPage(
         <h1 className="text-5xl font-bold">{product.name}</h1>
         <PriceTag price={product.price} className="mt-4" />
         <p className="py-6">{product.description}</p>
-        <AddToCartButton productId={product.id} />
+        <AddToCartButton
+          productId={product.id}
+          incrementProductQuantity={incrementProductQuantity} />
       </div>
     </div>
   )
