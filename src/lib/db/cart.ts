72888032a9cdad6fae8356394dcee77206c55cf4
Reyna May <roma.reynamayA@gmail.com>
2023-11-03 18:51:57 -0600

    Rename the collection

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,6 +1,17 @@
 //  functions we use in server actions
 import prisma from "./prisma";
 import {cookies} from "next/headers"
+
+// gets the cart from database
+export async function getCart() {
+  // get the cart id from the cookie
+  const localCartId = cookies().get("localCartId")?.value;
+  const cart = localCartId? await prisma.cart.findUnique({
+    where: {id: localCartId},
+    include: { items}
+  })
+}
+
 // create an empty cart
 export async function createCart() {
   const newCart = await prisma.cart.create({
@@ -8,5 +19,7 @@ export async function createCart() {
   })
 
   // use cookies for anonymous carts
+  //  Note: Needs encryption + secure settings in real production app
+  // Note: Needs to be a session cookie
   cookies().set("localCartId", newCart.id);
 }
